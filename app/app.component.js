a9818d2bc818b93fd099059f2361971a44efcc55
ajc388 <ajc388@nau.edu>
2016-04-19 15:55:32 -0700

    Completed minor refactoring as part 4 Multiple Components of the Angular2 tutorial

--- app/app.component.js
+++ app/app.component.js
@@ -1,4 +1,4 @@
-System.register(['angular2/core'], function(exports_1, context_1) {
+System.register(['angular2/core', './hero-detail.component'], function(exports_1, context_1) {
     "use strict";
     var __moduleName = context_1 && context_1.id;
     var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
@@ -10,20 +10,39 @@ System.register(['angular2/core'], function(exports_1, context_1) {
     var __metadata = (this && this.__metadata) || function (k, v) {
         if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
     };
-    var core_1;
-    var Hero, HEROES, AppComponent;
+    var core_1, hero_detail_component_1;
+    var AppComponent, HEROES;
     return {
         setters:[
             function (core_1_1) {
                 core_1 = core_1_1;
+            },
+            function (hero_detail_component_1_1) {
+                hero_detail_component_1 = hero_detail_component_1_1;
             }],
         execute: function() {
-            Hero = (function () {
-                function Hero() {
+            //{{}} One way data binding
+            //[(ngModel)] Two way data binding
+            //*ngFor='#var of list/array' iterate through list
+            //(function)=functionDef I.E (click)="onSelect(hero)"
+            AppComponent = (function () {
+                function AppComponent() {
+                    this.title = 'Tour of Heroes';
+                    this.heroes = HEROES;
                 }
-                return Hero;
+                AppComponent.prototype.onSelect = function (hero) { this.selectedHero = hero; };
+                AppComponent = __decorate([
+                    core_1.Component({
+                        selector: 'my-app',
+                        template: "\n\t    <h1>{{title}}</h1>\n\t    <h2>My Heroes</h2>\n\t    <ul class=\"heroes\">\n\t      <li *ngFor=\"#hero of heroes\"\n\t        [class.selected]=\"hero === selectedHero\"\n\t        (click)=\"onSelect(hero)\">\n\t        <span class=\"badge\">{{hero.id}}</span> {{hero.name}}\n\t      </li>\n\t    </ul>\n\t    <my-hero-detail [hero]=\"selectedHero\"></my-hero-detail>\n      ",
+                        styles: ["\n\t    .selected {\n\t      background-color: #CFD8DC !important;\n\t      color: white;\n\t    }\n\t    .heroes {\n\t      margin: 0 0 2em 0;\n\t      list-style-type: none;\n\t      padding: 0;\n\t      width: 15em;\n\t    }\n\t    .heroes li {\n\t      cursor: pointer;\n\t      position: relative;\n\t      left: 0;\n\t      background-color: #EEE;\n\t      margin: .5em;\n\t      padding: .3em 0;\n\t      height: 1.6em;\n\t      border-radius: 4px;\n\t    }\n\t    .heroes li.selected:hover {\n\t      background-color: #BBD8DC !important;\n\t      color: white;\n\t    }\n\t    .heroes li:hover {\n\t      color: #607D8B;\n\t      background-color: #DDD;\n\t      left: .1em;\n\t    }\n\t    .heroes .text {\n\t      position: relative;\n\t      top: -3px;\n\t    }\n\t    .heroes .badge {\n\t      display: inline-block;\n\t      font-size: small;\n\t      color: white;\n\t      padding: 0.8em 0.7em 0 0.7em;\n\t      background-color: #607D8B;\n\t      line-height: 1em;\n\t      position: relative;\n\t      left: -1px;\n\t      top: -4px;\n\t      height: 1.8em;\n\t      margin-right: .8em;\n\t      border-radius: 4px 0 0 4px;\n\t    }\n\t  "],
+                        directives: [hero_detail_component_1.HeroDetailComponent]
+                    }), 
+                    __metadata('design:paramtypes', [])
+                ], AppComponent);
+                return AppComponent;
             }());
-            exports_1("Hero", Hero);
+            exports_1("AppComponent", AppComponent); //onSelect function variable of type hero is the hero for databinding.
             HEROES = [
                 { "id": 11, "name": "Mr. Nice" },
                 { "id": 12, "name": "Narco" },
@@ -36,28 +55,6 @@ System.register(['angular2/core'], function(exports_1, context_1) {
                 { "id": 19, "name": "Magma" },
                 { "id": 20, "name": "Tornado" }
             ];
-            //{{}} One way data binding
-            //[(ngModel)] Two way data binding
-            //*ngFor='#var of list/array' iterate through list
-            //(function)=functionDef I.E (click)="onSelect(hero)"
-            //*ngIf="variable" then populate all nested elements in this block
-            AppComponent = (function () {
-                function AppComponent() {
-                    this.title = 'Tour of Heroes';
-                    this.heroes = HEROES;
-                }
-                AppComponent.prototype.onSelect = function (hero) { this.selectedHero = hero; };
-                AppComponent = __decorate([
-                    core_1.Component({
-                        selector: 'my-app',
-                        template: "\n\t    <h1>{{title}}</h1>\n\t    <h2>My Heroes</h2>\n\t    <ul class=\"heroes\">\n\t      <li *ngFor=\"#hero of heroes\"\n\t        [class.selected]=\"hero === selectedHero\"\n\t        (click)=\"onSelect(hero)\">\n\t        <span class=\"badge\">{{hero.id}}</span> {{hero.name}}\n\t      </li>\n\t    </ul>\n\t    <div *ngIf=\"selectedHero\">\n\t      <h2>{{selectedHero.name}} details!</h2>\n\t      <div><label>id: </label>{{selectedHero.id}}</div>\n\t      <div>\n\t        <label>name: </label>\n\t        <input [(ngModel)]=\"selectedHero.name\" placeholder=\"name\"/>\n\t      </div>\n\t    </div>\n      ",
-                        styles: ["\n\t    .selected {\n\t      background-color: #CFD8DC !important;\n\t      color: white;\n\t    }\n\t    .heroes {\n\t      margin: 0 0 2em 0;\n\t      list-style-type: none;\n\t      padding: 0;\n\t      width: 15em;\n\t    }\n\t    .heroes li {\n\t      cursor: pointer;\n\t      position: relative;\n\t      left: 0;\n\t      background-color: #EEE;\n\t      margin: .5em;\n\t      padding: .3em 0;\n\t      height: 1.6em;\n\t      border-radius: 4px;\n\t    }\n\t    .heroes li.selected:hover {\n\t      background-color: #BBD8DC !important;\n\t      color: white;\n\t    }\n\t    .heroes li:hover {\n\t      color: #607D8B;\n\t      background-color: #DDD;\n\t      left: .1em;\n\t    }\n\t    .heroes .text {\n\t      position: relative;\n\t      top: -3px;\n\t    }\n\t    .heroes .badge {\n\t      display: inline-block;\n\t      font-size: small;\n\t      color: white;\n\t      padding: 0.8em 0.7em 0 0.7em;\n\t      background-color: #607D8B;\n\t      line-height: 1em;\n\t      position: relative;\n\t      left: -1px;\n\t      top: -4px;\n\t      height: 1.8em;\n\t      margin-right: .8em;\n\t      border-radius: 4px 0 0 4px;\n\t    }\n\t  "]
-                    }), 
-                    __metadata('design:paramtypes', [])
-                ], AppComponent);
-                return AppComponent;
-            }());
-            exports_1("AppComponent", AppComponent); //onSelect function variable of type hero is the hero for databinding.
         }
     }
 });
--- app/app.component.ts
+++ app/app.component.ts
@@ -1,28 +1,11 @@
 import {Component} from 'angular2/core';
-
-export class Hero {
-	id: number;
-	name: string;
-}
-
-var HEROES: Hero[] = [
-      { "id": 11, "name": "Mr. Nice" },
-      { "id": 12, "name": "Narco" },
-      { "id": 13, "name": "Bombasto" },
-      { "id": 14, "name": "Celeritas" },
-      { "id": 15, "name": "Magneta" },
-      { "id": 16, "name": "RubberMan" },
-      { "id": 17, "name": "Dynama" },
-      { "id": 18, "name": "Dr IQ" },
-      { "id": 19, "name": "Magma" },
-      { "id": 20, "name": "Tornado" }
-    ];
+import {Hero} from './hero';
+import {HeroDetailComponent} from './hero-detail.component';
 
 //{{}} One way data binding
 //[(ngModel)] Two way data binding
 //*ngFor='#var of list/array' iterate through list
 //(function)=functionDef I.E (click)="onSelect(hero)"
-//*ngIf="variable" then populate all nested elements in this block
 @Component({
     selector: 'my-app',
     template:`
@@ -35,14 +18,7 @@ var HEROES: Hero[] = [
 	        <span class="badge">{{hero.id}}</span> {{hero.name}}
 	      </li>
 	    </ul>
-	    <div *ngIf="selectedHero">
-	      <h2>{{selectedHero.name}} details!</h2>
-	      <div><label>id: </label>{{selectedHero.id}}</div>
-	      <div>
-	        <label>name: </label>
-	        <input [(ngModel)]="selectedHero.name" placeholder="name"/>
-	      </div>
-	    </div>
+	    <my-hero-detail [hero]="selectedHero"></my-hero-detail>
       `,
     styles:[`
 	    .selected {
@@ -92,7 +68,8 @@ var HEROES: Hero[] = [
 	      margin-right: .8em;
 	      border-radius: 4px 0 0 4px;
 	    }
-	  `]
+	  `],
+	directives: [HeroDetailComponent]
 })
 export class AppComponent { 
 	title = 'Tour of Heroes';
@@ -100,3 +77,16 @@ export class AppComponent {
   	selectedHero: Hero;
   	onSelect(hero: Hero) { this.selectedHero = hero; }
 } //onSelect function variable of type hero is the hero for databinding.
+
+var HEROES: Hero[] = [
+      { "id": 11, "name": "Mr. Nice" },
+      { "id": 12, "name": "Narco" },
+      { "id": 13, "name": "Bombasto" },
+      { "id": 14, "name": "Celeritas" },
+      { "id": 15, "name": "Magneta" },
+      { "id": 16, "name": "RubberMan" },
+      { "id": 17, "name": "Dynama" },
+      { "id": 18, "name": "Dr IQ" },
+      { "id": 19, "name": "Magma" },
+      { "id": 20, "name": "Tornado" }
+    ];
